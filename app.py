bfe4422ce097c36ddd00bad15a2d30aea986f734
Santipap250 <santipap350z@gmail.com>
2026-02-11 16:31:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -421,6 +421,19 @@ def vtx():
     # หากต้องการส่งข้อมูลไดนามิก ให้เติม context dict
     return render_template("vtx.html")
 
+# ===============================
+# ERROR HANDLERS
+# ===============================
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
 # Run (dev)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
