a672508e5d48455c6f4d74e78b79d3db1f234174
Santipap250 <santipap350z@gmail.com>
2026-04-03 05:43:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -172,8 +172,6 @@ def _cells_from_str(s):
         return None
 
 # ── Flask app ─────────────────────────────────────────────────────────────
-from werkzeug.middleware.proxy_fix import ProxyFix
-
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
